615691d3ae3d67f9db582332a534be23969b5981
SleekPanther <macrobyte2@gmail.com>
2018-07-10 17:21:00 -0400

    Converted to pageAction so only appears on YouTube.com

--- background.js
+++ background.js
@@ -1,10 +1,28 @@
+//Only make extension icon visible on YouTube.com
+const youtubeBaseUrl = 'youtube.com'	//lowercase matters
+chrome.runtime.onInstalled.addListener(function() {
+	// Replace all rules ...
+	chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+		// With a new rule ...
+		chrome.declarativeContent.onPageChanged.addRules([{
+			conditions: [
+				new chrome.declarativeContent.PageStateMatcher({
+					pageUrl: { hostContains: youtubeBaseUrl },
+				})
+			],
+			// And shows the extension's page action
+			actions: [ new chrome.declarativeContent.ShowPageAction() ]
+		}])
+	})
+})
 
-const MAGIC_URL_POSTFIX = '&list=ULcxqQ59vzyTk'
-const YOUTUBE_VIDEO_URL_START = 'https://www.youtube.com/watch?v='
+
+const MAGIC_URL_POSTFIX = '&list=ULcxqQ59vzyTk'		//thing to append to make it play chronlogically
+const YOUTUBE_VIDEO_URL_START = 'https://www.youtube.com/watch?v='	//a youtube url without the videoID
 const regexMatchVideoId = /youtube.com\/watch\?v=([^&\?]*)/		//regular expression matches youtube.com/watch?v=[VIDEO_ID]	& captures the video ID since the ID is either the end of the string or ends at a question mark or ampersand
 
 //Browser action instead of popup
-chrome.browserAction.onClicked.addListener(tab=>{
+chrome.pageAction.onClicked.addListener(tab=>{
 	appendToUrl()
 })
 
@@ -14,7 +32,7 @@ function appendToUrl(){
 		let oldURL = currentTab.url
 		let videoId = oldURL.match(regexMatchVideoId)[1]	//get video id
 		let newUrl = YOUTUBE_VIDEO_URL_START + videoId + MAGIC_URL_POSTFIX
-		chrome.tabs.update(currentTab.id, {url: newUrl})
+		chrome.tabs.update(currentTab.id, {url: newUrl})	//update tab/reloads page with new location
 	})
 }
 
